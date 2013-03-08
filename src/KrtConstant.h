048e81025b09f0691319afc1f3be7d6175321f7a
Mr.Wid <mr_wid@163.com>
2013-03-08 18:32:39 +0800

    添加搜索对话框中按钮常量 ========================
    wid

--- src/KrtConstant.h
+++ src/KrtConstant.h
@@ -5,3 +5,8 @@
 // MainFrame
 #define		BTN_GETROOT			10000			//选择起始目录
 #define		BTN_START_SEARCH	10001			//开始搜索按钮
+
+
+// SearchingDlg
+#define		BTN_PAUSECONTINUE	10002			//暂停/继续按钮
+#define		BTN_STOPEXIT		10003			//停止/退出
